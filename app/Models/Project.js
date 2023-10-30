7cdd8215a73bc7369a51df2864b57eb7b671083a
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2023-10-30 19:00:47 -0300

    feat: create Task Model and create relationships

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -9,6 +9,10 @@ class Project extends Model {
     user () {
         return this.belongsTo('App/Models/User')
     }
+
+    task () {
+        return this.hasMany('App/Models/Task')
+    }
 }
 
 module.exports = Project
